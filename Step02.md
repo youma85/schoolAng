9305ba226b248972ff8f800c185c18fc264acb7b
Rafii <rafiiyoussef@gmail.com>
2020-04-27 17:38:47 +0000

    Adding classroom using local reference and event emitter

---
+++ Step02.md
@@ -0,0 +1,22 @@
+# Step 02
+
+**Adding Navigation to the Navbar** 
+
+In the Nav bar component adding click event to each link.
+When clicking to the link emit the page name to the appComponent  in order to render the selected component.
+
+**Using Item component to display Student Card**
+Passing Student to the Student Item using Property binding 
+
+**Show student data In the Student Details component** 
+When clicking on the Show button of The Student Item Pass the student to the Parents In order to show his informations in the Student Details Component   
+
+**Adding classroom using local reference and event emitter**
+
+Get data of inputs using local reference and viewChild.
+
+Emit the new classroom to the classroom list.
+
+**Adding Custom Directive**
+
+ Creat new directive to apply Shadow effect to card element
--- src/app/classroom/classroom-dialog/classroom-dialog.component.ts
+++ src/app/classroom/classroom-dialog/classroom-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Classroom} from "../classroom";
 
 @Component({
   selector: 'app-classroom-dialog',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classroom-dialog.component.scss']
 })
 export class ClassroomDialogComponent implements OnInit {
+  @ViewChild('cycleInput') cycleInput: ElementRef;
+  @ViewChild('labelInput') labelInput: ElementRef;
+  @ViewChild('levelInput') levelInput: ElementRef;
+  @Output() classroomAdded = new EventEmitter<Classroom>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSave() {
+    const cycle = this.cycleInput.nativeElement.value;
+    const label = this.labelInput.nativeElement.value;
+    const level = this.levelInput.nativeElement.value;
+
+    this.classroomAdded.emit(new Classroom(0, cycle, level, label));
+    this.cycleInput.nativeElement.value = '';
+    this.labelInput.nativeElement.value = '';
+    this.levelInput.nativeElement.value = '';
+  }
 }
