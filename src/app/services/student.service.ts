001e3cd675abc876f5dafab0c29a8c0c39be25e5
Rafii <rafiiyoussef@gmail.com>
2020-05-07 01:39:38 +0000

    Use firebase storage to store Images

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -18,13 +18,13 @@ export class StudentService {
     const classrooms = this.classroomService.getClassrooms();
     this.students = [
       new Student(0, 'Arbi', 'Ahmed', moment('20/03/2013', 'DD/MM/YYYY').toDate(),
-        'Casablanca', 'adr1', 'Casablanca', 'assets/img/boy1.png', classrooms[0]),
+        'Casablanca', 'adr1', 'Casablanca', 'images/boy1.png', classrooms[0]),
       new Student(1, 'Charaf', 'Hafssa', moment('13/12/2018', 'DD/MM/YYYY').toDate(),
-        'Casablanca', 'adr2', 'Casablanca', 'assets/img/girl.png', classrooms[1]),
+        'Casablanca', 'adr2', 'Casablanca', 'images/girl1.png', classrooms[1]),
       new Student(2, 'Jilali', 'Mourad', moment('13/12/2015', 'DD/MM/YYYY').toDate(),
-        'Casablanca', 'adr3', 'Casablanca', 'assets/img/boy2.png', classrooms[2]),
+        'Casablanca', 'adr3', 'Casablanca', 'images/boy2.png', classrooms[2]),
       new Student(3, 'Jilali', 'Mehdi', moment('13/12/2016', 'DD/MM/YYYY').toDate(),
-        'Casablanca', 'adr4', 'Casablanca', 'assets/img/boy2.png', classrooms[2]),
+        'Casablanca', 'adr4', 'Casablanca', 'images/boy1.png', classrooms[2]),
     ];
   }
 
